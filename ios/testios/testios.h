ff3c30f0633e8e910fa723d4bf0ad25a5adf88ff
xiayiyong <xiayiyong@chexiang.com>
2016-05-23 16:40:30 +0800

    测试提交

---
+++ ios/testios/testios.h
@@ -0,0 +1,19 @@
+//
+//  testios.h
+//  testios
+//
+//  Created by xiayiyong on 16/5/23.
+//  Copyright © 2016年 上海赛可电子商务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testios.
+FOUNDATION_EXPORT double testiosVersionNumber;
+
+//! Project version string for testios.
+FOUNDATION_EXPORT const unsigned char testiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testios/PublicHeader.h>
+
+
